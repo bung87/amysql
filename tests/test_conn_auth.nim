2ba38a2f73c2caf601ff33c2b0254ee99f746a74
bung87 <crc32@qq.com>
2020-08-14 12:07:42 +0800

    add caching_sha2 test

---
+++ tests/test_conn_auth.nim
@@ -0,0 +1,26 @@
+import unittest
+
+import async_mysql, asyncdispatch, asyncnet
+from nativesockets import AF_INET, SOCK_STREAM
+import unittest
+import net
+
+const database_name = "test"
+const port: int = 3306
+const host_name = "localhost"
+const ssl: bool = false
+
+proc doTCPConnect(user_name, pass_word:string): Future[Connection] {.async.} =
+  let sock = newAsyncSocket(AF_INET, SOCK_STREAM)
+  await connect(sock, host_name, Port(port))
+  if ssl:
+    when defined(ssl):
+      let ctx = newContext(verifyMode = CVerifyPeer)
+      return await establishConnection(sock, user_name, database=database_name, password = pass_word, ssl=ctx)
+  else:
+    return await establishConnection(sock, user_name, database=database_name, password = pass_word)
+
+suite "test connection auth methods":
+  test "caching_sha2":
+    let conn = waitFor(doTCPConnect("sha2user","123456"))
+    waitFor conn.close()
