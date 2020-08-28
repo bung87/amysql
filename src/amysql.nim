6e684648a23e7efbc7c5709c3e645d44c74960b7
bung87 <crc32@qq.com>
2020-08-28 17:00:31 +0800

    improve dbQuote

--- src/amysql.nim
+++ src/amysql.nim
@@ -932,10 +932,24 @@ proc selectDatabase*(conn: Connection, database: string): Future[ResponseOK] {.a
 
 proc dbQuote*(s: string): string =
   ## DB quotes the string.
-  result = "'"
+  result = newStringOfCap(s.len + 2)
+  result.add "'"
   for c in items(s):
-    if c == '\'': add(result, "''")
-    else: add(result, c)
+    # see https://cheatsheetseries.owasp.org/cheatsheets/SQL_Injection_Prevention_Cheat_Sheet.html#mysql-escaping
+    case c:
+      of '\0': result.add "\\0"
+      of '\b': result.add "\\b"
+      of '\t': result.add "\\t"
+      of '\l': result.add "\\n"
+      of '\r': result.add "\\r"
+      of '\x1a': result.add "\\Z"
+      of '"': result.add "\\\""
+      of '%': result.add "\\%"
+      of '\'': result.add "\\'"
+      of '\\': result.add "\\\\"
+      of '_': result.add "\\_"
+      of Letters+Digits: result.add c
+      else: result.add "\\" & c
   add(result, '\'')
 
 proc dbFormat(formatstr: SqlQuery, args: varargs[string]): string =
